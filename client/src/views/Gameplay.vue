606fb243eef7ecc35fd06f7f478c6df3330f91de
dhimasadip <dhimasadip.dap@gmail.com>
2020-06-25 19:15:28 +0700

    visual card

--- client/src/views/Gameplay.vue
+++ client/src/views/Gameplay.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="text-center mt-5">
+        <div class="text-center mt-3">
             <h2 class="text-secondary">Goodluck, {{ $store.state.nickname }}!</h2>
         </div>
         <div class="d-flex justify-content-center mt-5 text-center">
@@ -11,17 +11,17 @@
                     <span class="badge badge-secondary pb-1">Sneakers</span>
                     <div class="d-flex justify-content-around mt-3">
                         <div class="shadow rounded">
-                            <p class="bg-light">Real Price</p>
-                            <p class="card-text pt-0 p-2">Rp. 2.990.000</p>
+                            <p class="bg-carrot pb-1 mb-0">Real Price</p>
+                            <p class="card-text text-secondary pt-0 p-2" style="text-decoration: line-through;">Rp. 2.990.000</p>
                         </div>
                         <div class="shadow rounded">
-                            <p class="bg-light">Sale Price</p>
-                            <p class="card-text pt-0 p-2">Rp. 2.2?0.000</p>
+                            <p class="bg-carrot pb-1 mb-0">Sale Price</p>
+                            <p class="card-text text-carrot pt-0 p-2">Rp. 2.2?0.000</p>
                         </div>
                     </div>
                     <div class="mt-3 d-flex">
-                        <input class="form-control" type="number" min="0" v-model="price" placeholder="guess price">
-                        <button class="ml-3 btn btn-primary">Submit</button>
+                        <input class="form-control shadow-sm" type="number" min="0" v-model="price" placeholder="guess price">
+                        <button class="ml-3 btn btn-primary shadow">Submit</button>
                     </div>
                 </div>
             </div>
